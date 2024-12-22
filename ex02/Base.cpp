328d95624c5985ed97e9a22681dacb40f639cb51
tkubanyc <tkubanyc@student.42heilbronn.de>
2024-12-22 20:19:43 +0100

    ex02: chore: Create Base.cpp.

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -1,28 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Base.hpp                                           :+:      :+:    :+:   */
+/*   Base.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/22 20:08:30 by tkubanyc          #+#    #+#             */
-/*   Updated: 2024/12/22 20:18:41 by tkubanyc         ###   ########.fr       */
+/*   Created: 2024/12/22 20:17:51 by tkubanyc          #+#    #+#             */
+/*   Updated: 2024/12/22 20:18:45 by tkubanyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BASE_HPP
-#define BASE_HPP
+#include "Base.hpp"
 
-#include <string>
-#include <iostream>
-#include "colors.hpp"
-
-class	Base {
-
-public:
-
-	virtual	~Base( void );
-
-};
-
-#endif
+Base::~Base( void ) {}
