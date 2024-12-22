c97729ae2a3813c2f08c0cc61219670d47778739
tkubanyc <tkubanyc@student.42heilbronn.de>
2024-12-22 21:26:37 +0100

    ex02: chore: Create main function

---
+++ ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/22 20:45:46 by tkubanyc          #+#    #+#             */
+/*   Updated: 2024/12/22 21:25:13 by tkubanyc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.hpp"
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+int	main( void ) {
+
+	Base*	base = generate();
+
+	std::cout << "Identified by pointer: ";
+	identify( base );
+
+	std::cout << "Identified by reference: ";
+	identify( *base );
+
+	delete base;
+
+	return 0;
+}
